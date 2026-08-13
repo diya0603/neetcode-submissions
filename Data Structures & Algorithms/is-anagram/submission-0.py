513f2e266a565d9ffcbdd19f62a60af58f591197
Diya Shetty <120095535+diya0603@users.noreply.github.com>
2026-08-13 12:08:28 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        map1=defaultdict(int)
+        map2=defaultdict(int)
+        if len(s)!=len(t):
+            return False
+        for i in range(len(s)):
+            map1[s[i]]+=1
+            map2[t[i]]+=1
+        if map1==map2:
+            return True
+        else:
+            return False
