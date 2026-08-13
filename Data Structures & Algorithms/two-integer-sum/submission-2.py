f0fa16bfd45cca6b62358f4e8e9c360f75c498ab
Diya Shetty <120095535+diya0603@users.noreply.github.com>
2026-08-13 12:22:21 -0400

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        compliment=defaultdict(int)
+        for i in range(len(nums)):
+            comp=target-nums[i]
+            if comp in compliment:
+                return [compliment[comp],i]
+            compliment[nums[i]]=i
+           
